6e56f710bb42389bea52743ab7353e8dd2639d32
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-05 19:53:40 -0300

    Criar os estilos do projeto

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router";
 import Ordenes from "./components/paginas/Ordenes";
 import Menu from "./components/paginas/Menu";
 import NuevoPlatillo from "./components/paginas/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Ordenes />} />
         <Route path="/menu" element={<Menu />} />
---
+++ src/components/ui/Sidebar.js
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
+      <div className="p-6 ">
+        <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">
+          RestaurantApp
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
