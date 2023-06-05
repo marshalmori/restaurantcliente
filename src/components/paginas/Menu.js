a108203fbc7ac082006885668f55a21fe943ba54
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-05 19:44:34 -0300

    Criar rotas e os componentes Menu e Novo prato

---
+++ src/components/paginas/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-light mb-4">Menu</h1>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ src/components/paginas/NuevoPlatillo.js
@@ -0,0 +1,9 @@
+const NuevoPlatillo = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-light mb-4">Nuevo Platillo</h1>
+    </div>
+  );
+};
+
+export default NuevoPlatillo;
