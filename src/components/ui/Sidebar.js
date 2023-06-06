63f7eaf0d06aecee095c2971f23ec5e150d11198
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-05 21:57:12 -0300

    Router para Novos Pratos

--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 const Sidebar = () => {
   return (
     <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
@@ -5,6 +7,30 @@ const Sidebar = () => {
         <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">
           RestaurantApp
         </p>
+        <p className="mt-3 text-gray-600">
+          Administra tu restaurant en las siguientes opciones:
+        </p>
+
+        <hr className="mt-5" />
+
+        <nav className="mt-10">
+          <NavLink
+            className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+            activeClassName="bg-red-500"
+            exact
+            to="/"
+          >
+            Ordenes
+          </NavLink>
+          <NavLink
+            className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900"
+            activeClassName="text-yellow-500"
+            exact
+            to="/menu"
+          >
+            Menú
+          </NavLink>
+        </nav>
       </div>
     </div>
   );
