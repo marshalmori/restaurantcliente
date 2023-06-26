80a4570e6cc99f65ce11e427db3d547139362d82
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-06-26 13:30:41 -0300

    Consultar o Firebase para obter as ordens de pedidos

--- src/components/paginas/Ordenes.js
+++ src/components/paginas/Ordenes.js
@@ -1,4 +1,40 @@
+import { useEffect, useState, useContext } from "react";
+import { FirebaseContext } from "../../firebase";
+import {
+  collection,
+  getDocs,
+  query,
+  getFirestore,
+  where,
+} from "firebase/firestore";
+
 const Ordenes = () => {
+  // context con las operaciones de firebase
+  const { firebase } = useContext(FirebaseContext);
+
+  // state con las ordenes
+  const [ordenes, guardarOrdenes] = useState([]);
+
+  useEffect(() => {
+    obtnerOrdenes();
+  }, []);
+
+  const obtnerOrdenes = async () => {
+    const q = query(
+      collection(firebase.db, "ordenes"),
+      where("completado", "==", false)
+    );
+    const querySnapshot = await getDocs(q);
+    const ordenes = querySnapshot.forEach((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+
+    guardarOrdenes(ordenes);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-light mb-4">Ordenes</h1>
